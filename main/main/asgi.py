372c6d960a99fc34664cac876fe38547c23defac
Vansh Jaiswal <vjas2606@gmail.com>
2026-08-29 17:30:26 +0530

    Websockets Entry Point

--- main/main/asgi.py
+++ main/main/asgi.py
@@ -7,10 +7,24 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
 """
 
-import os
+# core/asgi.py
 
+import os
+import django
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+import sessions_app
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+django.setup()
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    # Regular HTTP → standard Django views
+    'http': get_asgi_application(),
+
+    # WebSocket → Unity VR client connects here
+    'websocket': AuthMiddlewareStack(
+        URLRouter(sessions_app.routing.websocket_urlpatterns)
+    ),
+})
